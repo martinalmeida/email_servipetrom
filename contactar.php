d1bbcddf796ad7435b69229fb63de3724355b02b
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2021-04-02 18:10:57 -0500

    Add files via upload

--- contactar.php
+++ contactar.php
@@ -2,25 +2,32 @@
 <html lang="en">
   <head>
   </head>
-	<form name="contact" method="POST" data-netlify="true">
-	  <p>
-	    <label>Your Name: <input type="text" name="name" /></label>   
-	  </p>
-	  <p>
-	    <label>Your Email: <input type="email" name="email" /></label>
-	  </p>
-	  <p>
-	    <label>Your Role: <select name="role[]" multiple>
-	      <option value="leader">Leader</option>
-	      <option value="follower">Follower</option>
-	    </select></label>
-	  </p>
-	  <p>
-	    <label>Message: <textarea name="message"></textarea></label>
-	  </p>
-	  <p>
-	    <button type="submit">Send</button>
-	  </p>
+	<form name="contact" method="POST" data-netlify="true" class="sign-up-form" netlify>
+		<h2 class="title">Ponté en contacto:</h2>
+		<div class="input-field">
+		  <i class="fas fa-user"></i>
+		  <input type="text" placeholder="Usuario" />
+		</div>
+		<div class="input-field">
+		  <i class="fas fa-envelope"></i>
+		  <input type="text" placeholder="Mensaje" />
+		</div>
+		<input type="submit" class="btn" value="Enviar" />
+		<p class="social-text">Redes sociales</p>
+		<div class="social-media">
+		  <a href="https://www.facebook.com/martin.d.almeida.12/" class="social-icon">
+		    <i class="fab fa-facebook-f"></i>
+		  </a>
+		  <a href="https://www.instagram.com/martin_almeida2/" class="social-icon">
+		    <i class="fab fa-instagram"></i>
+		  </a>
+		  <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" class="social-icon">
+		    <i class="fab fa-google"></i>
+		  </a>
+		  <a href="#" class="social-icon">
+		    <i class="fab fa-linkedin-in"></i>
+		  </a>
+		</div>
 	</form>
   </body>
 </html>
