0c8af04ff258fccc995646813df8e46cc5211019
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2021-04-02 18:02:03 -0500

    Add files via upload

---
+++ contactar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  </head>
+	<form name="contact" method="POST" data-netlify="true" class="sign-up-form">
+		<h2 class="title">Ponté en contacto:</h2>
+		<div class="input-field">
+		  <i class="fas fa-user"></i>
+		  <input type="text" placeholder="Usuario" />
+		</div>
+		<div class="input-field">
+		  <i class="fas fa-envelope"></i>
+		  <input type="text" placeholder="Mensaje" />
+		</div>
+		<input type="submit" class="btn" value="Enviar" />
+		<p class="social-text">Redes sociales</p>
+		<div class="social-media">
+		  <a href="https://www.facebook.com/martin.d.almeida.12/" class="social-icon">
+		    <i class="fab fa-facebook-f"></i>
+		  </a>
+		  <a href="https://www.instagram.com/martin_almeida2/" class="social-icon">
+		    <i class="fab fa-instagram"></i>
+		  </a>
+		  <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" class="social-icon">
+		    <i class="fab fa-google"></i>
+		  </a>
+		  <a href="#" class="social-icon">
+		    <i class="fab fa-linkedin-in"></i>
+		  </a>
+		</div>
+	</form>
+  </body>
+</html>
